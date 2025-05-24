6e9628a3c06921b79c11615218856a0b49aefddb
Tarcisio <tarcisiomateus98@gmail.com>
2025-05-24 07:19:49 -0300

    completed use-cases/SingUp test

--- src/use-cases/org/SingUp.spec.ts
+++ src/use-cases/org/SingUp.spec.ts
@@ -2,8 +2,25 @@ import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-
 import { compare } from 'bcryptjs'
 import { expect, describe, it } from 'vitest'
 import { SingUpUseCase } from './SingUp'
+import { OrgAlreadyExistsError } from '../errors/org-already-exists-error'
 
 describe('SingUp Use Case', () => {
+  it('should be able to SingUP', async () => {
+    const orgsRepository = new InMemoryOrgsRepository()
+    const sut = new SingUpUseCase(orgsRepository)
+
+    const { org } = await sut.execute({
+      name: 'Org1',
+      email: 'org@gmail.com',
+      password: '123456',
+      address: 'main street studio 96',
+      zipCode: '123456',
+      whatsapp: '98765-4321',
+    })
+
+    expect(org.id).toEqual(expect.any(String))
+  })
+
   it('should hash org password upon registration', async () => {
     const orgsRepository = new InMemoryOrgsRepository()
     const sut = new SingUpUseCase(orgsRepository)
@@ -23,5 +40,32 @@ describe('SingUp Use Case', () => {
     )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should NOT be able to SingUP with same email twice', async () => {
+    const orgsRepository = new InMemoryOrgsRepository()
+    const sut = new SingUpUseCase(orgsRepository)
+
+    const email = 'org@gmail.com'
+
+    await sut.execute({
+      name: 'Org1',
+      email,
+      password: '123456',
+      address: 'main street studio 96',
+      zipCode: '123456',
+      whatsapp: '98765-4321',
+    })
+
+    await expect(() =>
+      sut.execute({
+        name: 'Org2',
+        email,
+        password: '123456',
+        address: 'main street studio 96',
+        zipCode: '123456',
+        whatsapp: '98765-4321',
+      }),
+    ).rejects.toBeInstanceOf(OrgAlreadyExistsError)
   })
 })
